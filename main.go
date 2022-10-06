c8a20b998d7ed010c9d9e402855bd17fe2594c19
noah <noahzajicek@gmail.com>
2022-10-06 12:18:46 -0400

    Added a contact page

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello! Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello! Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, `To get in touch, please send an email to <a
+		href=\"mailto:noahzajicek@gmail.com\">noahzajicek@gmail.com </a>.`)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
+
 }
