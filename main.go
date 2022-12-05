18b8edc971b6deb7f3ebf74f8f0128c68a389760
noah <noahzajicek@gmail.com>
2022-12-05 13:52:05 -0500

    Added the FAQ page for the application

--- main.go
+++ main.go
@@ -18,9 +18,15 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	href=\"mailto:noahzajicek@gmail.com\">noahzajicek@gmail.com </a>.`)
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "These are the most frequently asked questions: ")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
