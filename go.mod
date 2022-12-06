43cf1c83ad26f9ac252714975c62551bbe8df6bc
noah <noahzajicek@gmail.com>
2022-12-06 17:12:45 -0500

    New implemented router. HTTP router replacing gorilla mux

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module application.com/m0
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Hello! Welcome to my awesome site!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, `To get in touch, please send an email to <a
 	href=\"mailto:noahzajicek@gmail.com\">noahzajicek@gmail.com </a>.`)
 }
 
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, `<h1>Frequently asked questions:</h1><p>Here is a list of questions 
 	our users commonly ask.</p> `)
@@ -31,10 +32,10 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	r := mux.NewRouter()
-	r.NotFoundHandler = http.HandlerFunc(notFound)
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	http.ListenAndServe(":3000", r)
+	r := httprouter.New()
+	r.NotFound = http.HandlerFunc(notFound)
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.GET("/faq", faq)
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
